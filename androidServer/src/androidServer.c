c7f0aaf17f9cee0ae3d1f53c220a466d6b2da6ee
Kjær <crhk1998@gmail.com>
2020-06-17 15:32:21 +0200

    Comments and cleaned the code up

--- androidServer/src/androidServer.c
+++ androidServer/src/androidServer.c
@@ -6,25 +6,20 @@
 #include <sys/socket.h> 
 #include <sys/types.h> 
 #include "androidServer.h"
-#define MAX 64 
-#define PORT 8080
-#define SA struct sockaddr 
-int sockfd, connfd, len; 
-int connfd2;
+
+int sockfd, connfd, len, connfd2, connected;
 struct sockaddr_in servaddr, cli;
-int connected;
 
 
-// PORT 8080 til den ene og port 8181 til den anden.
 void startServer(){
     // socket create and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
+        printf("socket creation failed. \n"); 
         exit(0); 
     } 
     else
-        printf("Socket successfully created..\n"); 
+        printf("Socket successfully created. \n"); 
     bzero(&servaddr, sizeof(servaddr)); 
   
     // assign IP, PORT 
@@ -35,50 +30,56 @@ void startServer(){
     //Reuse address on reconnect.
     int reuse = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0)
-        perror("setsockopt(SO_REUSEADDR) failed");
+        perror("setsockopt(SO_REUSEADDR) failed. \n");
 
-#ifdef SO_REUSEPORT
+    //Reuse port on reconnect.
+    #ifdef SO_REUSEPORT
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, (const char*)&reuse, sizeof(reuse)) < 0) 
-        perror("setsockopt(SO_REUSEPORT) failed");
-#endif
+        perror("setsockopt(SO_REUSEPORT) failed. \n");
+    #endif
 
     // Binding newly created socket to given IP and verification 
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
+        printf("socket bind failed.\n"); 
         exit(0); 
     } 
     else
-        printf("Socket successfully binded..\n"); 
+        printf("Socket successfully binded.\n"); 
   
     // Now server is ready to listen and verification 
     if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
+        printf("Listen failed.\n"); 
         exit(0); 
     } 
     else
-        printf("Server listening..\n"); 
+        printf("Server listening.\n"); 
     len = sizeof(cli); 
 
     // Accept the data packet from client and verification
+    // This is for the sending server thread.
     connfd = accept(sockfd, (SA*)&cli, &len); 
     if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
+        printf("Send server thread acccept failed.\n"); 
     } else
-        printf("Send thread acccept the client...\n");
+        printf("Send server thread acccept the client.\n");
     connected = 1;
 }
 
 void serverReadFunc(int connfd2){
-    printf("--------Read Function -------\n");
     char buff[MAX];
     bzero(buff, MAX); 
     
     int valread;
     if(valread = read(connfd2, buff, sizeof(buff)) > 0){
+        // Upon closing the app it sends "EXIT"
+        // Match it, and close all connects and restart server if that is the case.
         if(strcmp("EXIT",buff) != 0){
-            printf("From AndroidAPP: %s\n", buff);
+            //If not, put something in buffer.
+            //For now just printing a message set in android studio on the other side.
+            printf("From APP: %s\n", buff);
         } else {
-            printf("AndroidAPP CLOSED: %s\n", buff);
+            //Close connections & restart
+            printf("ANDROID APP CLOSED: %s\n", buff);
             close(sockfd);
             close(connfd);
             connected = 0;
@@ -90,18 +91,19 @@ void serverReadFunc(int connfd2){
 void serverRead(void *threadID){
     long tid;
     tid = (long)threadID;
-    printf("CREATED READ THREAD SUCCESFULLY! \n");
-  
-    printf("--------ReadThread -------\n");
+    printf("Created Read Server Thread! \n");
     // Accept the data packet from client and verification
     while(1){
         while(connected){
+        //Accept connect as it closes everytime for reading.
         connfd2 = accept(sockfd, (SA*)&cli, &len); 
         if (connfd2 < 0) { 
-        printf("server acccept failed...\n"); 
-            } else
-        printf("Read server acccept the client...\n");
+            printf("Read server thread acccept failed. \n"); 
+        } else {
+            printf("Read server thread acccept the client. \n");
+        }
         serverReadFunc(connfd2);
+        //close this connection again
         close(connfd2);
     }
 }
@@ -109,23 +111,26 @@ void serverRead(void *threadID){
 }
 
 void serverSendFunc(int connfd){
-    printf("--------Send Function -------\n");
     char buff[MAX];
     bzero(buff, MAX); 
     int n;
     bzero(buff, sizeof(buff)); 
+    // For now enter a message to send manually, should be read from buffer.
+    // 00000001[message] to send to first parameter
+    // 00000010[message] to send to second parameter
+    // 00000011 to confirm changed encryption key
     printf("Enter the string : "); 
     n = 0; 
     while ((buff[n++] = getchar()) != '\n');
 
-    int val = write(connfd, buff, sizeof(buff));
-    printf("%d",val);
+    //Send precode and message
+    write(connfd, buff, sizeof(buff));
 }
 
 void serverSend(void *threadID){
     long tid;
     tid = (long)threadID;
-    printf("CREATED SEND THREAD SUCCESFULLY! \n");
+    printf("Created Send Server Thread! \n");
     while(1){
         while(connected){
         serverSendFunc(connfd);
@@ -138,6 +143,7 @@ void serverSend(void *threadID){
 int main() { 
 pthread_t threads[2];
 
+//Start server
 startServer();
 
 int t1;
--- androidServer/src/androidServer.h
+++ androidServer/src/androidServer.h
@@ -1,6 +1,16 @@
 #ifndef ANDROIDSERVER_H
 #define ANDROIDSERVER_H
+#define MAX 64 
+#define PORT 8080
+#define SA struct sockaddr
 
-void func(int sockfd);
+void startServer();
 
+void serverReadFunc(int connfd2);
+
+void serverRead(void *threadID);
+
+void serverSendFunc(int connfd);
+
+void serverSend(void *threadID);
 #endif
