5ac9ef0c387c9fb11855205db2105e18ec7f6799
Kjær <crhk1998@gmail.com>
2020-06-17 15:08:33 +0200

    Working Android Server

--- androidServer/src/androidServer.c
+++ androidServer/src/androidServer.c
@@ -7,15 +7,15 @@
 #include <sys/types.h> 
 #include "androidServer.h"
 #define MAX 64 
-#define PORT 8181
+#define PORT 8080
 #define SA struct sockaddr 
 int sockfd, connfd, len; 
 int connfd2;
-struct sockaddr_in servaddr, cli; 
+struct sockaddr_in servaddr, cli;
+int connected;
+
 
 // PORT 8080 til den ene og port 8181 til den anden.
-
-
 void startServer(){
     // socket create and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
@@ -32,6 +32,16 @@ void startServer(){
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     servaddr.sin_port = htons(PORT); 
   
+    //Reuse address on reconnect.
+    int reuse = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0)
+        perror("setsockopt(SO_REUSEADDR) failed");
+
+#ifdef SO_REUSEPORT
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, (const char*)&reuse, sizeof(reuse)) < 0) 
+        perror("setsockopt(SO_REUSEPORT) failed");
+#endif
+
     // Binding newly created socket to given IP and verification 
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
         printf("socket bind failed...\n"); 
@@ -48,28 +58,32 @@ void startServer(){
     else
         printf("Server listening..\n"); 
     len = sizeof(cli); 
-  
+
     // Accept the data packet from client and verification
-    while(connfd <= 0){
     connfd = accept(sockfd, (SA*)&cli, &len); 
     if (connfd < 0) { 
         printf("server acccept failed...\n"); 
-        continue;
-    } 
-    else
-        printf("server acccept the client...\n"); 
-    }
-
+    } else
+        printf("Send thread acccept the client...\n");
+    connected = 1;
 }
 
-void serverReadFunc(int connfd){
+void serverReadFunc(int connfd2){
     printf("--------Read Function -------\n");
     char buff[MAX];
     bzero(buff, MAX); 
     
     int valread;
-    if(valread = read(connfd, buff, sizeof(buff)) > 0){
-    printf("From AndroidAPP: %s\n", buff); 
+    if(valread = read(connfd2, buff, sizeof(buff)) > 0){
+        if(strcmp("EXIT",buff) != 0){
+            printf("From AndroidAPP: %s\n", buff);
+        } else {
+            printf("AndroidAPP CLOSED: %s\n", buff);
+            close(sockfd);
+            close(connfd);
+            connected = 0;
+            startServer();
+        }
     } 
 }
 
@@ -81,16 +95,16 @@ void serverRead(void *threadID){
     printf("--------ReadThread -------\n");
     // Accept the data packet from client and verification
     while(1){
+        while(connected){
         connfd2 = accept(sockfd, (SA*)&cli, &len); 
         if (connfd2 < 0) { 
-            printf("server acccept failed...\n");
-            close(connfd2);
-        } else {
-            printf("server acccept the client...\n"); 
-            serverReadFunc(connfd2);
-            close(connfd2);
-     }
+        printf("server acccept failed...\n"); 
+            } else
+        printf("Read server acccept the client...\n");
+        serverReadFunc(connfd2);
+        close(connfd2);
     }
+}
     pthread_exit(NULL);
 }
 
@@ -104,7 +118,8 @@ void serverSendFunc(int connfd){
     n = 0; 
     while ((buff[n++] = getchar()) != '\n');
 
-    write(connfd, buff, sizeof(buff));
+    int val = write(connfd, buff, sizeof(buff));
+    printf("%d",val);
 }
 
 void serverSend(void *threadID){
@@ -112,15 +127,12 @@ void serverSend(void *threadID){
     tid = (long)threadID;
     printf("CREATED SEND THREAD SUCCESFULLY! \n");
     while(1){
-    printf("--------SendThread -------\n");
-    
-    serverSendFunc(connfd);
-    
+        while(connected){
+        serverSendFunc(connfd);
+        }
     }
     pthread_exit(NULL);
 }
-
-
 
 // Driver function 
 int main() { 
@@ -139,7 +151,6 @@ t2 = pthread_create(&threads[1], NULL, serverSend, (void *)1);
 if (t2){
     printf("Unable to create send thread\n");
 }
-
 pthread_exit(NULL);
 
 } 
